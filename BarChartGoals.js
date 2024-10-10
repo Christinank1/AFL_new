7c3757849179483f1f9c6c520f4df0f07941ebee
Christinank1 <ckru0001@student.monash.edu>
2024-10-10 13:31:35 +1100

    Update BarChartGoals.js

--- BarChartGoals.js
+++ BarChartGoals.js
@@ -13,24 +13,24 @@ let goals = [12.0, 13.2, 13.4, 12.4, 11.0, 13.5, 12.1, 13.2, 13.6, 14.0, 10.6, 1
 
 // Same colors as other charts
 let colors = [
-    'rgba(255, 0, 0, 1)',      // Bombers (Red/Black)
+    'rgba(139, 0, 0, 0.7)',      // Bombers (Red/Black)
     'rgba(255, 102, 0, 1)',    // Giants (Orange/Charcoal)
     'rgba(0, 0, 255, 1)',      // Bulldogs (Blue/Red/White)
     'rgba(102, 0, 153, 1)',    // Dockers (Purple/White)
     'rgba(0, 0, 0, 1)',        // Saints (Black/White/Red)
     'rgba(255, 204, 0, 1)',    // Lions (Maroon/Blue/Gold)
-    'rgba(0, 0, 128, 1)',      // Crows (Blue/Red/Yellow)
+    'rgba(173, 216, 230, 1)',      // Crows (Blue/Red/Yellow)
     'rgba(128, 64, 0, 1)',     // Hawks (Brown/Gold)
-    'rgba(0, 51, 204, 1)',     // Blues (Navy Blue/White)
+    'rgba(0, 128, 0, 1)',     // Blues (Navy Blue/White)
     'rgba(255, 0, 0, 1)',      // Swans (Red/White)
     'rgba(0, 102, 204, 1)',    // Kangaroos (Blue/White)
-    'rgba(0, 51, 102, 1)',     // Cats (Navy Blue/White)
-    'rgba(255, 0, 0, 1)',      // Suns (Red/Yellow)
+    'rgba(144, 238, 144, 1)',     // Cats (Navy Blue/White)
+    'rgba(255, 192, 203, 1)',      // Suns (Red/Yellow)
     'rgba(0, 0, 102, 1)',      // Demons (Navy Blue/Red)
     'rgba(0, 128, 128, 1)',    // Power (Teal/Black/White)
-    'rgba(0, 0, 0, 1)',        // Magpies (Black/White)
-    'rgba(255, 204, 0, 1)',    // Tigers (Yellow/Black)
-    'rgba(0, 51, 153, 1)'      // Eagles (Blue/Gold)
+    'rgba(128, 128, 128, 1)',        // Magpies (Black/White)
+    'rgba(255, 255, 224, 1)',    // Tigers (Yellow/Black)
+    'rgba(255, 215, 0, 1)'      // Eagles (Blue/Gold)
 ];
 
 
